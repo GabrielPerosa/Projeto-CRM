2904663eec0e0236fc53bcd483654463c258e7b8
Anhaia09 <leticia.anhaia09@gmail.com>
2025-01-16 14:04:39 -0300

    feat:updated login screen

--- app/frontend/src/app/login/page.tsx
+++ app/frontend/src/app/login/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { useState } from "react";
+
 import { useRouter } from 'next/navigation';
 import axios from "axios";
 import { Dialog } from 'primereact/dialog';
@@ -26,113 +27,109 @@ export default function Profile() {
 
             console.log(nomeUsuario);
 
-            // Armazene o nome em localStorage ou em um estado global/contexto
+             // Armazene o nome em localStorage ou em um estado global/contexto
             localStorage.setItem('nomeUsuario', nomeUsuario);
 
 
             console.log('Usuário logado:', response.data);
             router.push('http://localhost:3000/home');
         } catch (error) {
-            setShowPopup(true);
-            setTimeout(() => setShowPopup(false), 2000);
+            setShowPopup(true); 
+            setTimeout(() => setShowPopup(false), 2000); 
         }
     };
 
     return (
-        <main className="flex justify-center items-center min-h-screen p-4">
-            <div
-                className="fixed inset-0 bg-cover bg-center bg-no-repeat"
-                style={{ backgroundImage: "url('images/login/painel-solar.jpeg')" }}
-            >
-                {/* Camada de Gradiente para contraste */}
-                <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+        <main className="flex justify-center items-center min-h-screen bg-slate-300 p-4">
+            <div className="relative flex flex-col md:flex-row w-full max-w-4xl h-auto md:h-3/4 bg-slate-800 rounded-lg shadow-lg overflow-hidden">
 
-                {/* Formulário Transparente */}
-                <div className="relative z-10 h-screen flex items-center justify-center">
-                    <div className="w-full max-w-md p-6 bg-white/50 backdrop-blur-md rounded-lg shadow-xl">
-                        <h2 className="text-3xl font-bold text-center text-black mb-6">Bem-Vindo!</h2>
+                {/* Imagem de Fundo (lado esquerdo) */}
+                <div className="hidden md:block w-full md:w-1/2 relative">
+                    <img
+                        src="images/login/painel.jpg"
+                        alt="Imagem de fundo"
+                        className="w-full h-full object-cover opacity-80"
+                    />
+                    <div className="absolute inset-0  via-transparent"></div>
+                </div>
 
-                        <form onSubmit={handleSubmit}>
-                            {/* Campo Email */}
-                            <div className="mb-4">
-                                <label htmlFor="email" className="text-black block mb-2 text-sm font-medium">E-mail</label>
+                {/* Seção do Formulário (lado direito) */}
+                <div className="w-full md:w-1/2 p-6 sm:p-8 bg-blue-600  flex flex-col justify-center">
+                    <h2 className="text-2xl md:text-3xl font-extrabold mb-4 md:mb-6 text-white text-center">Bem-Vindo!</h2>
+
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-4">
+                            <label htmlFor="email" className="text-white">Email</label>
+                            <input
+                                type="text"
+                                id="email"
+                                name="email"
+                                value={email}
+                                onChange={(e) => setUserEmail(e.target.value)}
+                                required
+                                className="w-full p-3 mt-2 bg-blue-200 text-white rounded-md"
+                                placeholder="Digite seu email"
+                            />
+                        </div>
+
+                        {/* Campo Senha */}
+                        <div className="mb-4">
+                            <label htmlFor="password" className="text-white block mb-2">Senha</label>
+                            <div className="relative w-full">
                                 <input
-                                    type="text"
-                                    id="email"
-                                    name="email"
-                                    value={email}
-                                    onChange={(e) => setUserEmail(e.target.value)}
-                                    required
-                                    className="w-full p-3 rounded-md bg-white/50 text-black placeholder-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                    placeholder="Digite seu email"
+                                    type={senhaVisible ? "text" : "password"}
+                                    id="password"
+                                    value={senha}
+                                    onChange={(e) => setSenha(e.target.value)}
+                                    className="w-full p-3 bg-blue-200 text-white rounded-md pr-10"
+                                    placeholder="Digite sua senha"
                                 />
+                                {/* Ícone para exibir ou ocultar a senha */}
+                                <span
+                                    onClick={() => setPasswordVisible(!senhaVisible)}
+                                    className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white cursor-pointer"
+                                >
+                                    <i className={`pi ${senhaVisible ? 'pi-eye-slash' : 'pi-eye'}`} />
+                                </span>
                             </div>
+                        </div>
 
-                            {/* Campo Senha */}
-                            <div className="mb-4">
-                                <label htmlFor="password" className="text-black block mb-2 text-sm font-medium">Senha</label>
-                                <div className="relative">
-                                    <input
-                                        type={senhaVisible ? "text" : "password"}
-                                        id="password"
-                                        value={senha}
-                                        onChange={(e) => setSenha(e.target.value)}
-                                        className="w-full p-3 rounded-md bg-white/50 text-black placeholder-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 pr-12"
-                                        placeholder="Digite sua senha"
-                                    />
-                                    <span
-                                        onClick={() => setPasswordVisible(!senhaVisible)}
-                                        className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white cursor-pointer"
-                                    >
-                                        <i className={`pi ${senhaVisible ? 'pi-eye-slash' : 'pi-eye'}`} />
-                                    </span>
-                                </div>
-                            </div>
+                        <div className="text-right mt-4">
+                            <a href="http://localhost:3000/forgotpassword" className="text-white hover:underline">Esqueci minha senha</a>
+                        </div>
 
-                            {/* Links */}
-                            <div className="text-right">
-                                <a href="http://localhost:3000/forgotpassword" className="text-black text-sm hover:underline">Esqueci minha senha</a>
-                            </div>
+                        <div className="text-center mt-8">
+                            <button type="submit" className="w-full p-3 rounded-md bg-gradient-to-r from-blue-300 to-blue-400 text-white font-semibold">
+                                Entrar
+                            </button>
+                        </div>
 
-                            {/* Botão Entrar */}
-                            <div className="text-center mt-8">
-                                <button
-                                    type="submit"
-                                    className="w-full p-3 rounded-md bg-[#003366] text-white font-semibold">
-                                    Entrar
-                                </button>
-                            </div>
-
-
-                            {/* Link para Registro */}
-                            <div className="text-center mt-4">
-                                <a href="http://localhost:3000/register" className="text-black text-sm hover:underline">Ainda não tenho uma conta</a>
-                            </div>
-                        </form>
-                    </div>
+                        <div className="text-center mt-6">
+                            <a href="http://localhost:3000/register" className="text-white hover:underline">Ainda não tenho uma conta</a>
+                        </div>
+                    </form>
                 </div>
             </div>
-
-            <Dialog
-                header=""
-                visible={showPopup}
-                style={{
-                    width: '25vw',
-                    textAlign: 'center',
-                    position: 'absolute',
-                    top: '2%',
-                    left: '89%',
-                    transform: 'translateX(-50%)',
-                    backgroundColor: 'rgba(255, 99, 71, 0.8)',
-                    borderRadius: '8px',
-                }}
-                draggable={false}
-                closable={false}
-                onHide={() => setShowPopup(false)}
+            <Dialog 
+            header="" 
+            visible={showPopup} 
+            style={{
+                width: '25vw',
+                textAlign: 'center',
+                position: 'absolute',
+                top: '2%',
+                left: '89%',
+                transform: 'translateX(-50%)', 
+                backgroundColor: 'rgba(255, 99, 71, 0.8)', 
+                borderRadius: '8px', 
+            }} 
+            draggable={false} 
+            closable={false} 
+            onHide={() => setShowPopup(false)}
             >
-                <div className="p-4 rounded-md flex items-center justify-center">
-                    <p className="text-sm text-white font-bold">E-mail ou Senha incorretos</p>
-                </div>
+            <div className="p-4 rounded-md flex items-center justify-center">
+                <p className="text-sm text-white font-bold">E-mail ou Senha incorretos</p>
+            </div>
             </Dialog>
         </main>
     );
