ad1044662958cfd21a862c760950b435ecc61fd6
GabrielPerosa <gabrielperosa493@gmail.com>
2025-02-03 15:36:41 -0300

    add pages adm

--- app/frontend/src/app/pages/admin/services/page.tsx
+++ app/frontend/src/app/pages/admin/services/page.tsx
@@ -5,17 +5,17 @@ import Sidebar from '@/components/Sidebar';
 //import '../globals.css';
 import ServiceTable from '@/components/ServiceTable';
 
-export default function Myservices() {
+export default function Services() {
   return (
     <div className="flex h-screen bg-gray-100 space-x-18">
       <div className="w-64 bg-gray-100 shadow-md">
-        <Sidebar title="Meus serviços" username="Usuário" />
+        <Sidebar title="Serviços" username="Usuário" />
       </div>
 
       {/* MAIN */}
       <div className="flex-1 p-6">
         
-      <h1 className="text-2xl font-semibold mb-6 text-gray-800">Meus Serviços</h1>
+      <h1 className="text-2xl font-semibold mb-6 text-gray-800">Serviços</h1>
         
         <div className="w-full">
           <ServiceTable />
